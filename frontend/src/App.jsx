365d8ba779726e7d43e54f98ed8fb3423d5aaadb
Miran <miranfirdausi027@gmail.com>
2025-08-16 16:20:55 +0530

    feat: add profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import OAuthCallback from "./pages/OAuthCallback";
 import ZerodhaCallback from "./pages/ZerodhaCallback";
 import ChatInterface from "./components/ChatInterface";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   const [userData, setUserData] = useState({
@@ -37,6 +38,14 @@ function App() {
               element={
                 <ProtectedRoute>
                   <DashboardPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
                 </ProtectedRoute>
               }
             />
